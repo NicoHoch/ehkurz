20baae5f98a320da3a5444c0e1699d27d355c5bb
Nico Hoch <nico.hoch1@web.de>
2024-06-26 22:16:26 +0200

    added first langchain code

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,6 @@
-import dotenv from "dotenv";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import readline from "readline";
+
+import { langchainGemini } from "./langchainGemini";
 
 
-dotenv.config();
+langchainGemini();
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY as string);
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let isAwaitingResponse = false;
-
-async function run() {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-    const chat = model.startChat({
-        history: [],
-        generationConfig: {
-            maxOutputTokens: 1000
-        }
-    });
-
-    async function askAndRespond() {
-        if (!isAwaitingResponse) {
-            rl.question("You: ", async (input) => {
-                if (input === "exit") {
-                    rl.close();
-                } else {
-                    isAwaitingResponse = true;
-                    try {
-                        const result = await chat.sendMessageStream(input);
-                        let text = ""
-                        for await (const chunk of result.stream) {
-                            const chunkText = await chunk.text();
-                            console.log("Gemini: ", chunkText);
-                            text += chunkText;
-                        }
-                        isAwaitingResponse = false;
-                        askAndRespond();
-
-                    } catch (error) {
-                        console.error(error);
-                        isAwaitingResponse = false;
-                    }
-
-            }
-
-            });
-        } else {
-            console.log("Gemini: I am still thinking, please wait...")
-        }
-    }
-
-    askAndRespond();
-}
-
-run();
